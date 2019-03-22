865b81c8047a88b67636a85aba4f66d7e6e00a0f
williamhackett0 <48324932+williamhackett0@users.noreply.github.com>
2019-03-22 09:50:57 +0000

    Update magneto.ts

--- magneto.ts
+++ magneto.ts
@@ -4,6 +4,13 @@
 // Icon unicode characters can be found at: http://fontawesome.io/icons/
 //% color=190 weight=100 icon="\uf1ec" block="Magneto Blocks" advanced=false
 namespace magneto {
+     enum Direction {
+        Left = 0,
+        Right = 1,
+        Up = = 2,
+        Down = 3
+    }
+    
     /**
      * Test function for Magneto
      */
@@ -33,5 +40,45 @@ namespace magneto {
     //% shim=magneto::main_function
     export function main_function(): void{
         return;
+    }
+    
+    /**
+     * Device detected
+     */
+    //% weight=50
+    //% blockId=magneto_device_detected_event block="Micro:bit Detected" blockGap=8
+    //% shim=magneto::onDeviceDetected
+    export function onDeviceDetected(body: () => void): void{
+        return;   
+    }
+
+    /**
+     * Device detected in specific direction
+     */
+    //% weight=50
+    //% blockId=magneto_device_detected_direction_event block="Micro:bit Detected To My %direction" blockGap=8
+    //% shim=magneto::onDeviceInDirection
+    export function onDeviceDetectedDirection(direction : Direction, body: () => void): void{
+        return;   
+    }
+    
+    /**
+     * Device detected with name detected
+     */
+    //% weight=50
+    //% blockId=magneto_device_detected_name_event block="Micro:bit %name Detected" blockGap=8
+    //% shim=magneto::onDeviceDetectedName
+    export function onDeviceDetectedName(name : string, body: () => void): void{
+        return;   
+    }
+    
+    /**
+     * Device detected with name in a specific direction
+     */
+    //% weight=50
+    //% blockId=magneto_device_detected_name_direction_event block="Micro:bit %name Detected To My %direction" blockGap=8
+    //% shim=magneto::onDeviceDetectedNameDirection
+    function onDeviceDetectedNameDirection(name : string, direction : Direction, body: () => void): void{
+        return;   
     }
 }
